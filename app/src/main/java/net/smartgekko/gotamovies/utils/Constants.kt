1f277e3b0ee2029d918ed069a88dc689dc1e3a3f
RealVolAnd <out_of_bound@yahoo.com>
2021-06-26 17:36:48 +0300

    Delayed SplashScreen

--- app/src/main/java/net/smartgekko/gotamovies/utils/Constants.kt
+++ app/src/main/java/net/smartgekko/gotamovies/utils/Constants.kt
@@ -2,6 +2,6 @@ package net.smartgekko.gotamovies.utils
 
 import net.smartgekko.gotamovies.BuildConfig
 
-const val TMDB_BASE_URL = "https://api.nytimes.com/svc/movies/v2/reviews/all.json/"
+const val NYT_BASE_URL = "https://api.nytimes.com/svc/movies/v2/reviews/all.json/"
 const val nytApiKey = BuildConfig.NYT_API_KEY
-const val SPLASH_DISPLAY_LENGTH:Long = 3000
+const val SPLASH_DISPLAY_LENGTH:Long = 1500
